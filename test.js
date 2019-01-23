d063d88ebe3dea1d233567e5553b3e9c8d4f9dbe
spennyL92 <spencer.linn@pcc.edu>
2019-01-23 13:29:54 -0800

    Adding triangles

--- test.js
+++ test.js
@@ -1,15 +1,41 @@
-
-
 function setup() {
-	createCanvas(640, 480)
+  createCanvas(640, 480);
 }
 
 function draw() {
+  background(255, 23, 245);
 	if (mouseIsPressed){
-		fill(0);
+		fill(0);		
+		
 	} else {
-		fill(255);
+		noFill(255);
 	}
-
-	ellipse(50, 50, 50, 50,)
+	
+	
+	beginShape();
+	vertex(0, 400);
+	vertex(201, 80);
+	vertex(180, 312);
+	endShape(CLOSE)
+	
+	beginShape();
+	vertex(298, 170)
+	vertex(363, 25)
+	vertex(444, 375)
+	endShape(CLOSE)
+		
+		beginShape();
+	vertex(30, 275);
+	vertex(488, 96);
+	vertex(242, 328);
+	endShape(CLOSE)
+		
+		beginShape();
+	vertex(80, 470);
+	vertex(488, 380);
+	vertex(450, 470);
+	endShape(CLOSE)
+	
+	
 }
+
